51c5793c2ae58727ca0b7141bf982f0a610eec1f
Brian Kaufman <bpkaufman34@gmail.com>
2025-06-12 15:40:17 -0500

    vite gtag

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-PTZP6ZCXBW'
+      }
+    })
+  ],
 })
